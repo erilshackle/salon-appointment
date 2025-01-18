4b493d5ea33d160f828c6b6fedf9c6f01430319f
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-01-18 21:52:38 -0100

    delete project django

--- backend/agenda/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import ServicoViewSet, HorarioDeAtendimentoViewSet, AgendamentoViewSet
-
-router = DefaultRouter()
-router.register(r'servicos', ServicoViewSet, basename='servicos')
-router.register(r'horarios', HorarioDeAtendimentoViewSet, basename='horarios')
-router.register(r'agendamentos', AgendamentoViewSet, basename='agendamentos')
-
-urlpatterns = router.urls
